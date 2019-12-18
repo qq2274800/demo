970561b3294e804f3969d7a221038b18fe3d5edc
fangyurui <yurui.fang@ndmicro.com>
2019-12-18 13:45:12 +0800

    ceshi

--- src/main/java/com/example/demo/test/Test.java
+++ src/main/java/com/example/demo/test/Test.java
@@ -16,14 +16,14 @@ public class Test {
         filter(list);
     }
 
-    public static void find(List<String> list){
+    public static void find(List<String> list) {
         Map<String, List<String>> map = list.stream().collect(Collectors.groupingBy(str -> {
             String s = str.substring(1);
             return s.substring(2, 3);
         }));
     }
 
-    public static void filter(List<String> list){
+    public static void filter(List<String> list) {
         List<String> nList = list.stream().filter(str -> !str.equals("chengdu")).collect(Collectors.toList());
         System.out.println(nList);
     }
